e4f048f8198edc1279bbf822ac177ec3d87515ef
Daniel Adedeji <adedejiayokunledaniel@gmail.com>
2019-12-26 11:31:30 +0100

    initial  commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <div class="cropper-area"> 
+      <vue-cropper
+        :aspect-ratio="16 / 9"
+        ref="cropper"
+        :src="imgSrc"
+        alt="Source Image"
+        preview=".preview"
+        :cropmove="imageInfo"
+      >
+      </vue-cropper>
+      <button @click="cropImage" type="button">
+        Crop
+      </button>
+    </div>
+    <div class="cropped-area">
+      <img
+        v-if="cropImg" 
+        :src="cropImg"
+        class="cropped-img"
+        alt="Cropped Image" 
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
+
 export default {
+  components: { VueCropper },
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      cropImg: '',
+      imgSrc: "https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3289&q=80"
+    }
+  },
+  methods: {
+    imageInfo(){
+
+    },
+    cropImage(){
+      this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+button {
+  padding: 10px 20px;
+  background: aquamarine;
+  border: none;
+  border-radius: 5px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .main {
+    display: flex;
+
+  }
+
+  .cropper-area {
+    flex: 1;
+  }
+
+  .cropped-area {
+    flex: 1;
+  }
+  .cropped-img {
+    width: 100%;
+  }
 </style>
